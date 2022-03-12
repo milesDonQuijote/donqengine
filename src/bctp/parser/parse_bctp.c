dcf78834054b69af2b3dfcbaa48adf0f840628cb
milesdonquijote <milesdonquijote@gmail.com>
2022-03-12 15:05:46 +0300

    fixed bugs (sign up & substr)

--- src/bctp/parser/parse_bctp.c
+++ src/bctp/parser/parse_bctp.c
@@ -8,6 +8,5 @@ t_parsed_request	parse_bctp(void *request, unsigned int req_len)
 	req_c = (char *)request;
 	if (*req_c > REQTYPES_LEN)
 		return ((t_parsed_request){0, (void *)0});
-	write(1, "im going\n", 8);
 	return ((t_parsed_request){(int)*req_c, (runreq[*req_c])(request, req_len)});
 }
